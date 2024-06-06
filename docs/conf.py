fb9386f23af64de0497a101c033fd256fe6efe77
Abol <107064351+AbolDev@users.noreply.github.com>
2024-06-06 20:15:28 +0330

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,23 +1,60 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-project = 'Your Project Name'
-author = 'Your Name'
-release = '0.1'
+# -- Project information -----------------------------------------------------
+
+project = 'AparatLib'
+author = 'Abol'
+release = '0.4.1'
+
+# -- General configuration ---------------------------------------------------
 
 extensions = [
-    'myst_parser',
+    'myst_parser',  # Extension for parsing Markdown files
+    'sphinx.ext.autodoc',  # Include documentation from docstrings
+    'sphinx.ext.napoleon',  # Support for NumPy and Google style docstrings
+    'sphinx.ext.viewcode',  # Add links to highlighted source code
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+# -- Options for HTML output -------------------------------------------------
+
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# -- Options for source file suffixes ----------------------------------------
 
 source_suffix = {
     '.rst': 'restructuredtext',
     '.txt': 'restructuredtext',
     '.md': 'markdown',
 }
+
+# -- Options for autodoc extension -------------------------------------------
+
+# Order in which members are documented
+autodoc_member_order = 'bysource'
+
+# -- Options for Napoleon extension ------------------------------------------
+
+# Use Google style docstrings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
